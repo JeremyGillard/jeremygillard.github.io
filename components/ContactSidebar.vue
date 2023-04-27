dc75b1f836d3fc60d9882e2d4b819f72df08d3ab
Jeremy Gillard <gillard.jeremy@outlook.fr>
2023-04-27 21:09:58 +0200

    Revival with vuejs and css: ContactSidebar.vue

---
+++ components/ContactSidebar.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="sidebar">
+    <ul>
+      <li>
+        <ContactButton
+          type="fa-brands"
+          id="fa-github"
+          url="https://github.com/JeremyGillard"
+        />
+      </li>
+      <li>
+        <ContactButton
+          type="fa-brands"
+          id="fa-linkedin"
+          url="https://www.linkedin.com/in/jeremy-gillard/"
+        />
+      </li>
+      <li>
+        <ContactButton
+          type="fa-solid"
+          id="fa-envelope"
+          url="mailto:gillard.jeremy@outlook.fr"
+        />
+      </li>
+    </ul>
+  </div>
+</template>
+<script></script>
+<style scoped>
+.sidebar {
+  /* border: 1px solid black; */
+  position: absolute;
+  right: 40px;
+}
+ul {
+  display: flex;
+  flex-direction: column;
+  gap: 32px;
+}
+</style>
