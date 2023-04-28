26c8b2c84cb8ffc30c644f42b64f1482581ae20b
Jeremy Gillard <gillard.jeremy@outlook.fr>
2023-04-28 19:09:22 +0200

    Revival with vuejs and css: ContactButton v1 with transition

--- components/ContactButton.vue
+++ components/ContactButton.vue
@@ -1,31 +1,52 @@
 <template lang="">
-  <div class="contactButton">
+  <div class="contact-button">
     <a target="_blank" :href="url">
       <i :class="[type, id]"></i>
+      <p>{{ label }}</p>
     </a>
   </div>
 </template>
+
 <script>
 export default {
   props: {
+    label: String,
     type: String,
     id: String,
     url: String,
   },
 };
 </script>
+
 <style scoped>
-.contactButton {
-  /* border: 1px solid black; */
+.contact-button {
   text-align: center;
-  width: 24px;
-  font-size: 24px;
+  font-size: 1.2rem;
 }
+
+i {
+  text-align: center;
+  font-size: 2.8em;
+}
+
+p {
+  font-weight: bold;
+  position: relative;
+  top: -0.6em;
+  opacity: 0;
+  transition: all 200ms ease-in;
+}
+
 a {
   color: #838383;
 }
 
 a:hover {
   color: #383838;
+}
+
+a:hover p {
+  top: 0.2em;
+  opacity: 1;
 }
 </style>
