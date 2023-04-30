e939a621e520ddf9050daa0fcb2d0f015a1c1c58
Jeremy Gillard <gillard.jeremy@outlook.fr>
2023-05-01 00:17:18 +0200

    Revival with vuejs and css: DecodedText.vue

---
+++ components/DecodedText.vue
@@ -0,0 +1,23 @@
+<template>
+  <span class="decoded-text">
+    <DecodedLetter v-for="letter in lettersFromString">
+      {{ letter }}
+    </DecodedLetter>
+  </span>
+</template>
+
+<script>
+export default {
+  props: {
+    text: String,
+  },
+  computed: {
+    lettersFromString() {
+      return [...this.text];
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
