02fe0733113af8c8701e834bbdde55a9bc65b469
Jeremy Gillard <gillard.jeremy@outlook.fr>
2023-05-02 13:13:22 +0200

    refactor[error.vue]: try new styles

--- error.vue
+++ error.vue
@@ -1,42 +1,26 @@
 <template>
-  <main>
-    <div class="content">
-      <div class="title">
-        <h1>{{ error.statusCode }}</h1>
-        <h2>{{ error.message }}</h2>
-      </div>
-      <button @click="handleError">Go back home</button>
+  <div class="content">
+    <div class="title">
+      <h1>{{ error.statusCode }}</h1>
+      <h2>{{ error.message }}</h2>
     </div>
-  </main>
+    <a @click="handleError">Go back home</a>
+  </div>
 </template>
 
-<script setup>
-const props = defineProps({
-  error: Object,
-});
+<script setup lang="ts">
+const error = useError();
 
 const handleError = () => clearError({ redirect: "/" });
 </script>
 
 <style scoped>
-body {
-  margin: 0;
-  padding: 0;
-  box-sizing: inherit;
-  font-size: 0.625rem;
-  box-sizing: border-box;
-  font-family: "Quicksand", sans-serif;
-  color: #303030;
-  width: 100%;
-  height: 100vh;
-  background: #f7f7f7;
-}
 .content {
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  gap: 4rem;
+  gap: 8rem;
   height: 100vh;
 }
 
@@ -45,25 +29,19 @@ body {
 }
 
 h1 {
-  font-family: "Quicksand", sans-serif;
   font-weight: 500;
-  font-size: 5.6rem;
+  font-size: var(--text-size-xl);
   text-align: center;
 }
 
 h2 {
-  font-family: "Quicksand", sans-serif;
   font-weight: 500;
-  font-size: 3.2rem;
-  letter-spacing: 0.2rem;
-  word-spacing: 0.4rem;
-  overflow: hidden;
+  font-size: var(--text-size-l);
 }
 
-button {
-  font-family: "Quicksand", sans-serif;
+a {
   position: relative;
-  font-size: 1.8rem;
+  font-size: var(--text-size-l);
   color: #303030;
   animation-name: appear;
   animation-duration: 1s;
@@ -73,7 +51,7 @@ button {
   background-color: #f7f7f7;
 }
 
-button::before {
+a::before {
   content: "";
   position: absolute;
   display: block;
@@ -87,7 +65,7 @@ button::before {
   transition: transform 0.3s ease;
 }
 
-button:hover::before {
+a:hover::before {
   transform: scaleX(1);
 }
 </style>
