471c50178a9cb47bf26c0edd9f199a2af83da80b
Jeremy Gillard <gillard.jeremy@outlook.fr>
2023-05-02 15:34:20 +0200

    refactor[components/Navbar.vue]: About/Timeline first icon preview

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -2,17 +2,42 @@
   <nav class="navbar">
     <ul>
       <li>
-        <NuxtLink class="projects" to="/projects">Projects</NuxtLink>
+        <NuxtLink class="projects normal-link" to="/projects"
+          >Projects</NuxtLink
+        >
       </li>
       <li>
-        <NuxtLink class="about" to="/about">About</NuxtLink>
+        <NuxtLink class="about cta" to="/about">
+          About
+          <svg
+            class="svg"
+            width="31"
+            height="14"
+            viewBox="0 0 31 14"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M2 2L14.3095 11.1182C15.0167 11.642 15.9833 11.642 16.6905 11.1182L29 2"
+              stroke="#303030"
+              stroke-width="3"
+              stroke-linecap="round"
+            />
+          </svg>
+        </NuxtLink>
       </li>
       <li>
-        <NuxtLink class="notes" to="/notes">Notes</NuxtLink>
+        <NuxtLink class="notes normal-link" to="/notes">Notes</NuxtLink>
       </li>
     </ul>
   </nav>
 </template>
+
+<script setup lang="ts">
+// import { gsap } from "gsap";
+
+// gsap.to(".svg", { y: "0.8rem", duration: 1 });
+</script>
 
 <style scoped>
 ul {
@@ -23,10 +48,14 @@ ul {
 a {
   position: relative;
   font-size: 2.4rem;
-  color: #303030;
+  color: var(--primary-color);
   animation-name: appear;
   animation-duration: 1s;
   animation-fill-mode: backwards;
+}
+
+a:focus {
+  outline-color: var(--primary-color);
 }
 
 .about {
@@ -41,12 +70,12 @@ a {
   animation-delay: 4.6s;
 }
 
-a::before {
+.normal-link::before {
   content: "";
   position: absolute;
   display: block;
   width: 100%;
-  height: 0.15rem;
+  height: 0.3rem;
   bottom: 0;
   left: 0;
   border-radius: 2rem;
@@ -55,8 +84,26 @@ a::before {
   transition: transform 0.3s ease;
 }
 
-a:hover::before {
+.normal-link:hover::before {
   transform: scaleX(1);
+}
+
+.cta {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+.cta:hover .svg {
+  transform: translateY(0.8rem);
+  /* margin-top: 0.8rem; */
+}
+
+.svg {
+  transform: translateY(0.4rem);
+  transition: transform 0.3s ease;
+  /* margin-top: 0.4rem; */
 }
 
 @keyframes appear {
