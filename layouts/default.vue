9b2fff01fd6e512ea6c1045065a7ab01f2417cb2
Jeremy Gillard <gillard.jeremy@outlook.fr>
2023-05-02 13:01:19 +0200

    feat[layouts/default.vue]: scaffold default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,35 +1,21 @@
 <template>
-  <main>
-    <slot />
-  </main>
+  <div class="layout">
+    <header>
+      <Navbar />
+    </header>
+    <main>
+      <slot />
+    </main>
+  </div>
 </template>
 
-<style>
-:root {
-  --primary-background: #f7f7f7;
-  /* --neutral 60, --primary 30, --cta 10 */
+<style scoped>
+.layout {
+  position: relative;
+  margin: 0 14vw;
+  border: solid 1px black;
 }
-
-*,
-*::before,
-*::after {
-  margin: 0;
-  padding: 0;
-  list-style: none;
-  text-decoration: none;
-}
-
-html {
-  box-sizing: inherit;
-  font-size: 0.625rem;
-}
-
-body {
-  box-sizing: border-box;
-  font-family: "Quicksand", sans-serif;
-  color: #303030;
-  width: 100%;
-  height: 100vh;
-  background: var(--primary-background);
+header {
+  margin: 6vh 0;
 }
 </style>
