9a0af917fedfe14516497981a69f44dce8b38b57
Jeremy Gillard <gillard.jeremy@outlook.fr>
2023-05-06 15:51:16 +0200

    feat[pages/notes.vue]: grid tests + path test

--- pages/notes.vue
+++ pages/notes.vue
@@ -1,8 +1,30 @@
 <template>
   <section class="content">
-    <h1>Notes</h1>
+    <div class="grid">
+      <section v-for="(job, index) in jobs" :key="index" :class="`s${index}`">
+        <h3>{{ job.title }}</h3>
+        <h4>{{ job.details }}</h4>
+        <p>{{ job.description }}</p>
+      </section>
+    </div>
   </section>
 </template>
+
+<script setup lang="ts">
+let jobs = ref([] as Job[]);
+
+interface Job {
+  title: string;
+  details: string;
+  description: string;
+}
+
+const { data } = await useAsyncData("jobs", () =>
+  queryContent("/jobs").findOne()
+);
+
+jobs = data.value!.body.map((elt: Job) => elt);
+</script>
 
 <style scoped>
 .content {
@@ -14,5 +36,56 @@ h1 {
   font-size: var(--text-size-xl);
   margin-bottom: 4.8vh;
   text-align: center;
+}
+
+h3 {
+  font-family: Roboto, sans-serif;
+  font-size: 2.4rem;
+}
+
+h4 {
+  font-family: Roboto, sans-serif;
+  font-size: 1.8rem;
+  margin-bottom: 1rem;
+}
+
+p {
+  font-family: Roboto, sans-serif;
+  font-size: 1.4rem;
+  text-align: justify;
+}
+
+.grid {
+  display: grid;
+  gap: 3.2rem;
+  grid-template-columns: repeat(3, 1fr);
+  /* grid-template-rows: repeat(3, 1fr); */
+}
+
+.s0 {
+  grid-column: span 2;
+}
+
+.s1 {
+  grid-column-start: 2;
+  grid-column-end: 4;
+}
+
+.s2 {
+  grid-column: span 2;
+}
+
+.s3 {
+  grid-column-start: 2;
+  grid-column-end: 4;
+}
+
+.s4 {
+  grid-column: span 2;
+}
+
+.s5 {
+  grid-column-start: 2;
+  grid-column-end: 4;
 }
 </style>
