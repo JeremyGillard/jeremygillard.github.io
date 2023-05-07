1f76db90334db706643d0166d8e8784c0e9f8b60
Jeremy Gillard <gillard.jeremy@outlook.fr>
2023-05-07 18:44:12 +0200

    refactor[pages/notes.vue]: remove timeline sections + apply layout

--- pages/notes.vue
+++ pages/notes.vue
@@ -1,28 +1,11 @@
 <template>
-  <section class="content">
-    <div class="grid">
-      <TimelineSection
-        v-for="(job, index) in jobs"
-        :key="index"
-        :title="job.title"
-        :details="job.details"
-        :description="job.description"
-        :tags="job.tags"
-      />
-    </div>
-  </section>
+  <NuxtLayout>
+    <main class="content">
+      <h1>Notes</h1>
+    </main>
+  </NuxtLayout>
 </template>
 
-<script setup lang="ts">
-import { Job } from "~~/utils/timelinetypes";
-
-let jobs = ref([] as Job[]);
-
-const { data } = await useAsyncData("jobs", () =>
-  queryContent("/jobs").findOne()
-);
-
-jobs = data.value!.body.map((elt: Job) => elt);
-</script>
+<script setup lang="ts"></script>
 
 <style scoped></style>
