9e20e262e93a5ea1ccad64bfb0065f196fc90a43
Jeremy Gillard <gillard.jeremy@outlook.fr>
2023-05-10 18:49:12 +0200

    feat[Notes]: add notes home page

---
+++ components/content/NotesNavbar.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="notes-navbar">
+    <ul>
+      <li v-for="notes in navigation[1].children" :key="notes._path">
+        <NuxtLink :to="notes._path">{{ notes.title }}</NuxtLink>
+        <ul>
+          <li v-for="childnote in notes.children" :key="childnote._path">
+            <span>---</span
+            ><NuxtLink :to="childnote._path">{{ childnote.title }}</NuxtLink>
+          </li>
+        </ul>
+      </li>
+    </ul>
+    <!-- <pre>{{ navigation[1] }}</pre> -->
+  </div>
+</template>
+
+<script setup lang="ts">
+const { page, navigation } = useContent();
+
+console.log(navigation.value);
+console.log(page.value);
+</script>
+
+<style scoped></style>
