6781e36cc5aec9fc7339323f64fcd7e1118d4db8
Jeremy Gillard <gillard.jeremy@outlook.fr>
2023-05-20 13:21:32 +0200

    feat[global:style]: move css global style to assets

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -23,6 +23,9 @@ export default defineNuxtConfig({
           ]
         }
     },
+    css: [
+        '@/assets/css/global.css'
+    ],
     routeRules: {
       '/projects': { ssr: false },
       '/notes': { ssr: false }
