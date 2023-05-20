524f9309477e6f7ec7e16816d026ca645474b67d
Jeremy Gillard <gillard.jeremy@outlook.fr>
2023-05-20 19:08:31 +0200

    refactor[Navbar]: move toggleIndexRoute function next to useIndexRoute definition

--- composables/state.ts
+++ composables/state.ts
@@ -1,1 +1,10 @@
 export const useIndexRoute = () => useState<string>(() => '/#about');
+
+export function toggleIndexRoute() {   
+    const indexRoute = useIndexRoute();
+    if (indexRoute.value == "/#about") {
+        indexRoute.value = "/";
+    } else {
+        indexRoute.value = "/#about";
+    }
+}
